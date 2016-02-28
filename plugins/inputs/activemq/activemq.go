f12d4b87b9c9e029d051c5dc312114fbc2632849
Zane <zane.williamson@gmail.com>
2016-02-27 23:30:25 -0800

    updating activemq plugin

--- plugins/inputs/activemq/activemq.go
+++ plugins/inputs/activemq/activemq.go
@@ -1,105 +1,159 @@
-package activemq
+package activemq 
 
 import (
-	"bytes"
 	"encoding/xml"
-        "net/http"
 	"fmt"
-	"os/exec"
+	"net/http"
 	"strconv"
-	"strings"
+	"time"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
-	"golang.org/x/net/html/charset"
 )
 
-const DefaultURL = "http://localhost:8161/admin/xml/queues.jsp"
+const DefaultUsername = "admin"
+const DefaultPassword = "admin"
+const DefaultURL = "http://localhost:8161"
 
-type activemq struct {
-	URL string
-        Port string
-        Secure bool
-        User string
-        Password string
+type ActiveMQ struct {
+	URL      string
+	Name     string
+	Username string
+	Password string
+	Queues   []string
+
+	Client *http.Client
 }
 
-type queue struct  {
+type Queue  struct {
     Name string `xml:"name,attr"`
-}
-
-type stats struct {
     Size string `xml:"size,attr"`
     ConsumerCount string `xml:"consumerCount,attr"`
     EnqueueCount string `xml:"enqueueCount,attr"`
     DequeueCount string `xml:"dequeueCount,attr"`
 }
 
+type gatherFunc func(r *ActiveMQ, acc telegraf.Accumulator, errChan chan error)
+
+var gatherFunctions = []gatherFunc{gatherQueues}
+
 var sampleConfig = `
-	# Server to gather XML statistics from 
-	#
-	# If no host is specified, then uses localhost 
-	server = "localhost"
-        # Admin server port, defaults to 8161
-        port   = "8161"
-        # Set to true if authentication is required for admin interface, defaults to false
-        secure = true
-        # Set user and password to auth with, if admin interface is secure
-        user = "admin"
-        password = "admin"
+  url = "http://localhost:8161" # required
+  # name = "amq-server-1" # optional tag
+  # username = "admin"
+  # password = "admin"
 `
 
-func (r *activemq) SampleConfig() string {
+func (r *ActiveMQ) SampleConfig() string {
 	return sampleConfig
 }
 
-func (r *activemq) Description() string {
-	return "Read metrics from Activemq admin xml response"
+func (r *ActiveMQ) Description() string {
+	return "Read metrics from one ActiveMQ server via the admin API"
 }
 
-func (g *activemq) Gather(acc telegraf.Accumulator) error {
-        resp, err := http.Get("http://localhost:8161/admin/xml/queues.jsp")
-        resp.Header.Add("Accept", "application/xml")
-        resp.Header.Add("Content-Type","application/xml; charset=utf-8")
+func (r *ActiveMQ) Gather(acc telegraf.Accumulator) error {
+	if r.Client == nil {
+		r.Client = &http.Client{}
+	}
 
-        if err != nil {
-	    return err
-        }
+	var errChan = make(chan error, len(gatherFunctions))
 
-        defer resp.Body.Close()
+	for _, f := range gatherFunctions {
+		go f(r, acc, errChan)
+	}
 
-        out, err := ioutil.ReadAll(resp.Body) 
+	for i := 1; i <= len(gatherFunctions); i++ {
+		err := <-errChan
+		if err != nil {
+			return err
+		}
+	}
 
-	if err = importMetric(out, acc); err != nil {
+	return nil
+}
+
+func (r *ActiveMQ) requestXML(u string, target interface{}) error {
+	u = fmt.Sprintf("%s%s", r.URL, u)
+
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
 		return err
 	}
 
+	username := r.Username
+	if username == "" {
+		username = DefaultUsername
+	}
+
+	password := r.Password
+	if password == "" {
+		password = DefaultPassword
+	}
+
+	req.SetBasicAuth(username, password)
+
+	resp, err := r.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	xml.NewDecoder(resp.Body).Decode(target)
+
 	return nil
 }
 
-func importMetric(stat []byte, acc telegraf.Accumulator) error {
-	var queue stats
-
-	decoder := xml.NewDecoder(bytes.NewReader(stat))
-	decoder.CharsetReader = charset.NewReaderLabel
-	if err := decoder.Decode(&p); err != nil {
-		return fmt.Errorf("Cannot parse input with error: %v\n", err)
+func gatherQueues(r *ActiveMQ, acc telegraf.Accumulator, errChan chan error) {
+	// Gather information about queues
+	queues := make([]Queue, 0)
+	err := r.requestJSON("/admin/xml/queues.jsp", &queues)
+	if err != nil {
+		errChan <- err
+		return
 	}
 
-	fields := map[string]interface{}{
-                "queue_name":      queue.Name,
-		"queue_size":      stats.Size,
-		"consumer_count":  stats.ConsumerCount,
-		"enqueue_count":   stats.EnqueueCount,
-		"dequeue_count":   stats.DequeueCount,
-	}
-	acc.AddFields("activemq", fields)
+	for _, queue := range queues {
+		if !r.shouldGatherQueue(queue) {
+			continue
+		}
+		tags := map[string]string{
+			"queue":       queue.Name,
+		}
 
-	return nil
+		acc.AddFields(
+			"activemq_queue",
+			map[string]interface{}{
+				// common information
+				"size":                 queue.Size,
+				"consumer_count":       queue.ConsumerCount,
+				"enqueue_count":        queue.EnqueueCount,
+				"dequeue_count":        queue.DequeueCount,
+			},
+			tags,
+		)
+	}
+
+	errChan <- nil
+}
+
+func (r *ActiveMQ) shouldGatherQueue(queue Queue) bool {
+	if len(r.Queues) == 0 {
+		return true
+	}
+
+	for _, name := range r.Queues {
+		if name == queue.Name {
+			return true
+		}
+	}
+
+	return false
 }
 
 func init() {
 	inputs.Add("activemq", func() telegraf.Input {
-		return &activemq{}
+		return &ActiveMQ{}
 	})
 }
